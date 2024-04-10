84f2ee5921f830d1a5735c9de2af4e107612b075
KatelinVincent <katelin.vincent@aa.com>
2024-04-10 11:18:48 -0500

    Feat : header complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui:{
+    themes: [
+      "retro"
+    ]
+  }
 }
 
